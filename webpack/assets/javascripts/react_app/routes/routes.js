70e06bb43b0f52a5fcc69b54d713d91197cbbee4
Leos Stejskal <lstejska@redhat.com>
2021-03-04 12:41:23 +0100

    testing router

--- webpack/assets/javascripts/react_app/routes/routes.js
+++ webpack/assets/javascripts/react_app/routes/routes.js
@@ -2,5 +2,6 @@ import HostWizard from './HostWizard';
 import Audits from './Audits';
 import Models from './Models';
 import HostDetails from './HostDetails';
+import Testing from './Testing';
 
-export const routes = [HostWizard, Audits, Models, HostDetails];
+export const routes = [HostWizard, Audits, Models, HostDetails, Testing];
