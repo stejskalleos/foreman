9c5a6a13b358f7a521e6171b01187338a3fad2e3
Leos Stejskal <lstejska@redhat.com>
2021-03-04 13:21:45 +0100

    router

--- webpack/assets/javascripts/react_app/routes/Testing/TestingPage/index.js
+++ webpack/assets/javascripts/react_app/routes/Testing/TestingPage/index.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import { Form, FormSelect, FormSelectOption } from '@patternfly/react-core';
+import Select from '../../../components/common/forms/Select';
 import { translate as __ } from '../../../common/I18n';
 import PageLayout from '../../common/PageLayout/PageLayout';
 
-const TestingPage = () => {
+const TestingPage = ({props}) => {
   const [value, setValue] = useState('');
 
   const options = [
@@ -16,13 +17,25 @@ const TestingPage = () => {
     { value: 'other', label: 'Other', disabled: false },
   ];
 
-  console.log('TestingPage');
-  console.log(value);
+  const handler = (val) => {
+    console.log(val)
+    console.log(val)
+    console.log(val)
+    setValue(val);
+  }
 
+  // https://www.patternfly.org/2020.04/documentation/react/components/formselect#props
   return (
     <PageLayout header={__('Testing Page route')} searchable={false}>
+      Input
+      <input name="test-input" value={value} onChange={(e) => handler(e.target.value)} />
+      <hr />
+      Select:
+      <Select options={options} onChange={e => handler(e.target.value)}></Select>
+      <hr />
+      FormSelect:
       <Form>
-        <FormSelect value={value} onChange={e => setValue(e.target.value)}>
+        <FormSelect value={value} onChange={"NOT WORKING"}>
           {options.map((option, index) => (
             <FormSelectOption
               isDisabled={option.disabled}
